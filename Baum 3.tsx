ee5e49cef61b3e4f64d0cf7f8025c5dec41f2cf9
praktikum@linkemedienakademie.de <Hannah-prak>
2021-02-07 20:05:19 +0100

    tile 2

---
+++ Baum 3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Baum 3" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="https___computerwerk_maps_at_rc3_world__BUNDjugend_Baumhaus.png" width="128" height="128"/>
+</tileset>
